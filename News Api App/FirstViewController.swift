51274418e0b9258441a6fbeb502c8e8f199c34a4
vietnezo <viet@verve-inc.jp>
2018-06-20 18:38:16 +0900

    Initial Commit

---
+++ News Api App/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  News Api App
+//
+//  Created by VietNezo on 2018/06/20.
+//  Copyright © 2018 VietNezo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
